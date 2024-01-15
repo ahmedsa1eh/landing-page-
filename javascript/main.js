b20bce54f8f919a7630cd0a6b8d958485bfba9dd
ahmedsa1eh <156605596+ahmedsa1eh@users.noreply.github.com>
2024-01-15 15:50:16 +0200

    Add files via upload

---
+++ javascript/main.js
@@ -0,0 +1,6 @@
+// --------------------loader-----------------------
+var loader = document.getElementById("preloader");
+window.addEventListener("load", function () {
+  loader.style.display = "none";
+});
+// --------------------loader-----------------------
